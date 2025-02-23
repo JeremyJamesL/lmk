6980d70ce64c953529999ebb90ef23e2786f62f3
JeremyJamesL <jeremyluscombe@gmail.com>
2025-02-23 11:56:23 +0000

    pushed to staging

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,3 @@
-import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
-
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./dist/");
   eleventyConfig.addPassthroughCopy("./assets/");
