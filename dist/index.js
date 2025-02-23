210dee60d65e709cc5cb598cfff6b971a22aa3d6
JeremyJamesL <jeremyluscombe@gmail.com>
2025-02-23 16:24:36 +0000

    prod

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,16 @@
+const mobileMenu = document.getElementById("mobile-menu");
+const hamburgerLayers = document.querySelectorAll(".hamburger-layer");
+const header = document.querySelector("header");
 
+document.getElementById("hamburger").addEventListener("click", () => {
+  // Toggle open/close
+  if (mobileMenu.classList.contains("hidden")) {
+    mobileMenu.classList.replace("hidden", "flex");
+  } else {
+    mobileMenu.classList.replace("flex", "hidden");
+  }
+
+  if (header.classList.contains("text-white")) {
+    hamburgerLayers.forEach((el) => el.classList.toggle("bg-black!"));
+  }
+});
--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,13 @@
+import pluginSEO from "eleventy-plugin-seo";
+
 export default function (eleventyConfig) {
+  // Passthroughs
   eleventyConfig.addPassthroughCopy("./dist/");
   eleventyConfig.addPassthroughCopy("./assets/");
+
+  // Plugins
+  eleventyConfig.addPlugin(pluginSEO, {
+    description: "Lisa is a London based prop and interior stylist",
+    url: "https://lisa-mckinney.co.uk",
+  });
 }
